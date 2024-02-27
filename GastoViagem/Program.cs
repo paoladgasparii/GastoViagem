7aec1e12bcfa0788b357907e3b889e46090a3272
Usuario <Usuario@CPD03-M03>
2024-02-27 11:18:43 -0300

    Adicionar arquivos de projeto.

---
+++ GastoViagem/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GastoViagem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite o valor do litro do combustível: ");
+            double n1 = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite a distância percorrida: ");
+            double n2 = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite a quantidade de litros gastos por Km: ");
+            double n3 = double.Parse(Console.ReadLine());
+
+            double resultado = n2 / n3 * n1;
+
+            Console.WriteLine("O total gasto na viagem é: R$" + resultado);
+            Console.ReadKey();
+
+        }
+    }
+}
